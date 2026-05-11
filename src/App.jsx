6f54fdc6e40d579984ee08b20216ae1fd0d0d7fb
Matheus Carvalho Ventura <61662785356@espcid.intra>
2026-05-12 04:54:50 +0600

    Setup Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './pages/Home/Home'
+import Sobre from './pages/Sobre/Sobre'
+
+
+function App() {
+  return <>
+  <Home/>
+  <Sobre/></>
+}
+
+export default App
