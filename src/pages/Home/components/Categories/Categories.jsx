f1306b925edc08d704a7d7af2c3622e6ddf9bc42
Matheus Carvalho Ventura <61662785356@espcid.intra>
2026-05-16 06:29:02 +0600

    Feat: Mais do categories

--- src/pages/Home/components/Categories/Categories.jsx
+++ src/pages/Home/components/Categories/Categories.jsx
@@ -1,30 +1,17 @@
 import React from "react";
 import styles from "./Categories.module.scss";
 import { Icon } from "../Icons/Icons";
+import CategoriesIcon from "../CategoriesIcon/CategoriesIcon";
 const Categories = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Icon name="camisetas" color="#C92071git" />
-          <h2></h2>
-        </li>
-        <li>
-          <img src="" />
-          <h2></h2>
-        </li>
-        <li>
-          <img src="" />
-          <h2></h2>
-        </li>
-        <li>
-          <img src="" />
-          <h2></h2>
-        </li>
-        <li>
-          <img src="" />
-          <h2></h2>
-        </li>
+      <h2>Categorias</h2>
+      <ul className={`d-flex justify-content-center ${styles.categories}`} style={{backgroundColor:"chocolate"}}>
+        <CategoriesIcon name={'camisetas'} color={"#C92071"}/>
+        <CategoriesIcon name={'calcas'} color={"#C92071"}/>
+        <CategoriesIcon name={'headphones'} color={"#C92071"}/>
+        <CategoriesIcon name={'bones'} color={"#C92071"}/>
+        <CategoriesIcon name={'tenis'} color={"#C92071"}/>
       </ul>
     </div>
   );
